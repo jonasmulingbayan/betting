14556be7587fa8cb9b5c723ed82564c262e2b942
Jonas Isaiah Mulingbayan <jonasmulingbayan@gmail.com>
2024-06-07 16:18:35 +0800

    Inital Commit

---
+++ index.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+    $('#banner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+    });
+
+    $('#promotion').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 998,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+
+    var $populargamesSlider = $('#populargames');
+
+    // Check if #populargames slider exists
+    if ($populargamesSlider.length > 0) {
+        // Initialize the slick slider
+        $populargamesSlider.slick({
+            infinite: false,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            dots: false,
+            prevArrow: '<button type="button" class="slick-prev"></button>',
+            nextArrow: '<button type="button" class="slick-next"></button>',
+            centerMode: false, // Set centerMode to false
+            centerPadding: '0px', // Adjust the center padding
+            responsive: [
+                {
+                    breakpoint: 1500,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5,
+                    }
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
+        });
+    }
+
+    const iconsList = document.querySelectorAll('.icons');
+
+    iconsList.forEach(icon => {
+        icon.addEventListener('mouseenter', () => {
+            const infoHideContainer = icon.closest('.populargames-description').querySelector('.info-hide-container');
+            const desc = icon.closest('.populargames-description').querySelector('.desc');
+            infoHideContainer.style.display = 'block';
+            positionInfoHideContainer(infoHideContainer, desc);
+        });
+
+        icon.addEventListener('mouseleave', () => {
+            const infoHideContainer = icon.closest('.populargames-description').querySelector('.info-hide-container');
+            infoHideContainer.style.display = 'none';
+        });
+    });
+
+    function positionInfoHideContainer(container, desc) {
+        container.style.top = 0;
+        container.style.left = 0;
+        container.style.width = '100%';
+        container.style.height = '100%';
+        container.style.position = 'fixed';
+        container.style.background = 'rgba(0, 0, 0, 0.5)';
+        container.style.zIndex = '9999';
+    }
+
+});
